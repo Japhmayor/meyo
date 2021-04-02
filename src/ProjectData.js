aad173a2bb1a49c76eda01fff26bee8812054b80
Bontus <mrbjm1994@gmail.com>
2021-04-02 14:09:27 +0100

    updated my datas

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -6,7 +6,7 @@ const ProjectData = [
     github: "https://github.com/japhmayor/react-quiz-app",
     img: "./images/quiz.jpeg",
     website: "https://quizzical-einstein-4b55fb.netlify.app",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` A quiz app i built to learn how to consume API's in React. `,
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const ProjectData = [
     github: "https://github.com/Japhmayor/markdown-preview",
     img: "./images/markdown.jpeg",
     website: "https://silly-boyd-8a4648.netlify.app",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `I couldn't get myself to learn Markdown for my ReadMe's, so i decided to make a preview. `,
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const ProjectData = [
     github: "https://github.com/japhmayor/color-generator",
     img: "./images/color.jpeg",
     website: "https://silly-boyd-8a4648.netlify.app",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` This is a color generator built with react. I will discuss how i built this in a blogpost `,
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const ProjectData = [
     github: "https://github.com",
     img: "./images/item-7.jpeg",
     website: "https://github.com",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` This is what i thought off when i decided to build a fintech app. It's still in the works `,
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const ProjectData = [
     github: "https://github.com",
     img: "./images/item-7.jpeg",
     website: "https://github.com",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` This is still in the works. My own Youtube `,
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const ProjectData = [
     github: "#",
     img: "./images/item-7.jpeg",
     website: "#",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `I am still working on this using React 360 `,
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ const ProjectData = [
     github: "https://github.com/japhmayor/react-darkmode",
     img: "./images/darkmode.jpeg",
     website: "https://admiring-pare-144644.netlify.app/",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` Details will be in a blog post I will write soon `,
   },
   {
     id: 8,
@@ -69,7 +69,7 @@ const ProjectData = [
     github: "https://github.com/japhmayor/meyo-landing",
     img: "./images/landing.png",
     website: "https://meyolanding.netlify.app/",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` A User Interface Template a built in a boring day `,
   },
   {
     id: 9,
@@ -78,7 +78,7 @@ const ProjectData = [
     github: "https://github.com/japhmayor/meyo-dictionary",
     img: "./images/dict.png",
     website: "https://meyodictionary.netlify.app/",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` I built this dictionary whilst learning how to consume API's in Vue  `,
   },
   {
     id: 10,
@@ -87,16 +87,16 @@ const ProjectData = [
     github: "https://github.com/japhmayor/meyo",
     img: "./images/mine.png",
     website: "https://uyo-meyo.netlify.app/",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `This website you are viewing built with React. I didn't hard code anything, Everything is flexible and pluggable. You can clone the repo add your projects to ProjectData.js and your work experience to Data.js and your portfolio is ready `,
   },
   {
-    id: 10,
+    id: 11,
     title: "Note taking app",
     category: "Vue",
     github: "https://github.com/japhmayor/note-app",
     img: "./images/note.png",
     website: "https://notz.netlify.app/",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: ` A notetaking app for taking notes. I built this to learn how to create a PWA in Vue. `,
   },
 ];
 export default ProjectData;
